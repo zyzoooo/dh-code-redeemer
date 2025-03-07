2cb52a6b3855ca4c6edaab75de08ecb4bcdf1e58
zyzo <jayden0773@gmail.com>
2025-03-07 10:53:57 -0700

    Create codes.lua

---
+++ codes.lua
@@ -0,0 +1,20 @@
+return {
+    totalcash = 3690000,
+    credit = "FurryBoy",
+    codes = {
+        "TRADEME!",
+        "DAUP",
+        "pumpkins2023",
+        "RUBY",
+        "DACARNIVAL",
+        "HALLOWEEN2024",
+        "THANKSGIVING24",
+        "SHRIMP",
+        "VIP",
+        "2025",
+        "Beary",
+        "ShortCake",
+        "VALENTINES2025",
+        "BLOSSOM"
+    }
+}
